21ead21360b560b41a9aab1a56335a5cecc88e68
Wladimir Ferreira Gonçalves <codewlad@gmail.com>
2023-11-01 12:43:28 -0300

    Adjust text speed

--- web/src/components/Message/index.tsx
+++ web/src/components/Message/index.tsx
@@ -76,7 +76,7 @@ export const Message = ({
 			const timeoutId = setTimeout(() => {
 				setDisplayedMessage(message.slice(0, characterIndex + 1));
 				setCharacterIndex(characterIndex + 1);
-			}, 0);
+			}, 20);
 
 			return () => {
 				clearTimeout(timeoutId);
