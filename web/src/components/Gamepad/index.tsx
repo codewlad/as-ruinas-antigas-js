386891e1d664c70a200bfb2d1402b4ee3279c075
Wladimir Ferreira Gonçalves <codewlad@gmail.com>
2023-11-02 12:38:36 -0300

    Updated: Button press and hold event on mobile devices

--- web/src/components/Gamepad/index.tsx
+++ web/src/components/Gamepad/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { Conteiner, Sticky, Pad, Up, Left, Right, Down } from './styles';
 
 import imgUp from '@assets/arrow-up.svg';
@@ -22,15 +23,45 @@ export const Gamepad = ({
 		}
 	};
 
+	const [pressedButton, setPressedButton] = useState<string | null>(null);
+	const [pressedButtonInterval, setPressedButtonInterval] = useState<
+		number | undefined
+	>(0);
+
+	// Função para iniciar o movimento
+	const startMovement = (direction: string) => {
+		setPressedButton(direction);
+
+		// Executar ação imediatamente
+		handleMovement(direction, updateTorchPosition);
+
+		// Configurar um intervalo para continuar a ação enquanto o botão estiver pressionado
+		const interval = setInterval(() => {
+			handleMovement(direction, updateTorchPosition);
+		}, 100); // Chama a função a cada 100 milissegundos
+
+		// Salvar o ID do intervalo para limpar posteriormente
+		setPressedButtonInterval(interval);
+	};
+
+	// Função para parar o movimento quando o botão for solto
+	const stopMovement = () => {
+		if (pressedButton) {
+			clearInterval(pressedButtonInterval);
+			setPressedButton(null);
+		}
+	};
+
 	return (
 		<Conteiner>
 			<Sticky>
 				<Pad>
 					<Up>
 						<button
-							onClick={() =>
-								handleMovement('ArrowUp', updateTorchPosition)
-							}
+							onMouseDown={() => startMovement('ArrowUp')}
+							onMouseUp={stopMovement}
+							onTouchStart={() => startMovement('ArrowUp')}
+							onTouchEnd={stopMovement}
 							onKeyDown={preventSpacebarActivation}
 						>
 							<img
@@ -41,9 +72,10 @@ export const Gamepad = ({
 					</Up>
 					<Left>
 						<button
-							onClick={() =>
-								handleMovement('ArrowLeft', updateTorchPosition)
-							}
+							onMouseDown={() => startMovement('ArrowLeft')}
+							onMouseUp={stopMovement}
+							onTouchStart={() => startMovement('ArrowLeft')}
+							onTouchEnd={stopMovement}
 							onKeyDown={preventSpacebarActivation}
 						>
 							<img
@@ -54,12 +86,10 @@ export const Gamepad = ({
 					</Left>
 					<Right>
 						<button
-							onClick={() =>
-								handleMovement(
-									'ArrowRight',
-									updateTorchPosition
-								)
-							}
+							onMouseDown={() => startMovement('ArrowRight')}
+							onMouseUp={stopMovement}
+							onTouchStart={() => startMovement('ArrowRight')}
+							onTouchEnd={stopMovement}
 							onKeyDown={preventSpacebarActivation}
 						>
 							<img
@@ -70,9 +100,10 @@ export const Gamepad = ({
 					</Right>
 					<Down>
 						<button
-							onClick={() =>
-								handleMovement('ArrowDown', updateTorchPosition)
-							}
+							onMouseDown={() => startMovement('ArrowDown')}
+							onMouseUp={stopMovement}
+							onTouchStart={() => startMovement('ArrowDown')}
+							onTouchEnd={stopMovement}
 							onKeyDown={preventSpacebarActivation}
 						>
 							<img
